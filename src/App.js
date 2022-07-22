96499e8a41ff0e852a13bdf59c4bb94e5fa2ce84
Pannu786 <thepannu786@gmail.com>
2022-07-22 21:50:42 +0530

    all basic styling dne, all dependancy add

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className='App'>
+      <h1 className='underline text-3xl'>huh for real bra</h1>
     </div>
   );
 }
