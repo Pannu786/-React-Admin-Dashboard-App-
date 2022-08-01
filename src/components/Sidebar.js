741b0b417775d91196169a9dab3182e7920ac8cc
Pannu786 <thepannu786@gmail.com>
2022-08-01 13:32:51 +0530

    initial import dne with Sidebar cmp

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,4 +1,9 @@
 import { Link, NavLink } from 'react-router-dom';
+import { SiShopware } from 'react-icons/si';
+import { MdOutlineCancel } from 'react-icons/md';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+
+import { link } from '../data/dummy';
 
 const Sidebar = () => {
   return <div>Sidebar</div>;
