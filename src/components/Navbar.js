063762d99f826b42bce72b64dcba4e28a82e48ff
Pannu786 <thepannu786@gmail.com>
2022-08-03 18:03:29 +0530

    navbar import dne

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,3 +1,15 @@
+import { useEffect } from 'react';
+import { AiOutlineMenu } from 'react-icons/ai';
+import { FiShoppingCart } from 'react-icons/fi';
+import { BsChatLeft } from 'react-icons/bs';
+import { RiNotification3Line } from 'react-icons/ri';
+import { MdKeyboardArrowDown } from 'react-icons/md';
+import Tippy from '@tippyjs/react';
+import avatar from '../data/avatar.jpg';
+
+import { Chat, Cart, Notification, UserProfile } from '.';
+
+import { useStateContext } from '../contexts/ContextProvider';
 
 const Navbar = () => {
   return <div>Navbar</div>;
