b0de05f751d922b6ecb6675a9fa9f9bad70f14e8
Pannu786 <thepannu786@gmail.com>
2022-08-23 23:14:50 +0530

    basic navbar styling dne

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 import './App.css';
 
 const App = () => {
+  const activeMenu = false;
   return (
     <div>
       <BrowserRouter>
@@ -20,6 +21,22 @@ const App = () => {
                 <FiSettings />
               </button>
             </TooltipComponent>
+          </div>
+          {activeMenu ? (
+            <div className='w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white'>
+              Sidebar
+            </div>
+          ) : (
+            <div className='w-0 dark:bg-secondary-dark-bg'>Sidebar w-0</div>
+          )}
+          <div
+            className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${
+              activeMenu ? 'md:ml-72 ' : ' flex-2 '
+            }`}
+          >
+            <div className='fixed md:static bg-main-bg dark:bg-main-bg navbar w-full'>
+              Navbar
+            </div>
           </div>
         </div>
       </BrowserRouter>
