95174d6c83a4d4f70ab5fadc6db57bb75f1aa37b
Pannu786 <thepannu786@gmail.com>
2022-09-18 18:52:43 +0530

    Customers cmp initial dne

--- src/pages/Customers.js
+++ src/pages/Customers.js
@@ -1,7 +1,42 @@
-import React from 'react';
+import {
+  GridComponent,
+  ColumnDirective,
+  ColumnsDirective,
+  Page,
+  Selection,
+  Inject,
+  Edit,
+  Toolbar,
+  Sort,
+  Filter,
+} from '@syncfusion/ej2-react-grids';
 
-const Customer = () => {
-  return <div>Customer</div>;
+import { customersData, customersGrid } from '../data/dummy';
+import { Header } from '../components';
+
+const Customers = () => {
+  return (
+    <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
+      <Header category='Page' title='Customers' />
+      <GridComponent
+        dataSource={customersData}
+        allowPaging
+        allowSorting
+        toolbar={['Delete']}
+        editSettings={{ allowDeleting: true, allowEditing: true }}
+        width='auto'
+      >
+        <ColumnsDirective>
+          {customersGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject
+          services={[Page, Toolbar, Toolbar, Selection, Edit, Sort, Filter]}
+        />
+      </GridComponent>
+    </div>
+  );
 };
 
-export default Customer;
+export default Customers;
