70e878c2468132bced7bbbcefc5dc97b2ab161f2
Pannu786 <thepannu786@gmail.com>
2022-09-22 10:14:00 +0530

    all dne

--- src/components/charts/Linechart.js
+++ src/components/charts/Linechart.js
@@ -15,7 +15,11 @@ import {
   LinePrimaryXAxis,
 } from '../../data/dummy';
 
+import { useStateContext } from '../../contexts/ContextProvider';
+
 const LineChart = () => {
+  const { currentMode } = useStateContext();
+
   return (
     <ChartComponent
       id='line-chart'
@@ -24,6 +28,7 @@ const LineChart = () => {
       primaryYAxis={LinePrimaryYAxis}
       chartArea={{ border: { width: 0 } }}
       tooltip={{ enable: true }}
+      background={currentMode === 'Dark' ? '#33373E' : '#fff'}
     >
       <Inject services={[LineSeries, DateTime, Legend, Tooltip]} />
       <SeriesCollectionDirective>
